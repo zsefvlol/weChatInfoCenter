96fa40f8c1f7624fbaf58f0c5adfa6a67def8f98
lol.wen <zsefvlol@gmail.com>
2013-04-17 21:41:53 +0800

    change baike to wap

--- ThinkPHP/Extend/Vendor/BaiduBaike.php
+++ ThinkPHP/Extend/Vendor/BaiduBaike.php
@@ -6,17 +6,17 @@ class BaiduBaike{
 		$header = get_headers($url);
 		foreach ($header as $k=>$v)
 			if (strpos( $v , 'Location:')!==false)
-			$baikeUrl = 'http://baike.baidu.com'.trim(str_replace('Location:', '', $v));
+			$baikeUrl = 'http://wapbaike.baidu.com'.trim(str_replace('Location:', '', $v));
 		$html = file_get_contents($baikeUrl);
 		phpQuery::newDocumentHTML($html);
 		$title = htmlspecialchars_decode(pq('h1.title:first')->html());
-		$summary = htmlspecialchars_decode(strip_tags(pq('.card-summary-content:first')->html()));
-		if (!$summary) $summary = htmlspecialchars_decode(strip_tags(pq('div.para:first')->html()));
-		$img = pq('img.card-image:first')->attr('src');
-		if (!$img) $img = pq('img.editorImg:first')->attr('data-src');
+		$summary = htmlspecialchars_decode(strip_tags(pq('.summary:first')->html()));
+		if (!$summary) $summary = htmlspecialchars_decode(strip_tags(pq('content:first')->html()));
+		$img = pq('img-box img:first')->attr('src');
+		//if (!$img) $img = pq('img.editorImg:first')->attr('data-src');
 		return $title ? array(
-				'title'=>$title,
-				'summary'=>$summary,
+				'title'=>trim($title),
+				'summary'=>trim($summary),
 				'img'=>$img,
 				'baikeUrl'=>$baikeUrl) : false;
 	}
