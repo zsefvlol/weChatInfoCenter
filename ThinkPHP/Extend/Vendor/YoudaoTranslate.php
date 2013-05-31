48401e987a704868670163ec8fe0aeca2ce2a2e6
lol.wen <zsefvlol@163.com>
2013-05-31 16:26:44 +0800

    encode

--- ThinkPHP/Extend/Vendor/YoudaoTranslate.php
+++ ThinkPHP/Extend/Vendor/YoudaoTranslate.php
@@ -1,7 +1,7 @@
 <?php
 class YoudaoTranslate{
 	public static function translate($keyWord){
-		$url = 'http://fanyi.youdao.com/openapi.do?keyfrom=weChatInfoCenter&key=594387319&type=data&doctype=json&version=1.1&q=' . $keyWord;
+		$url = 'http://fanyi.youdao.com/openapi.do?keyfrom=weChatInfoCenter&key=594387319&type=data&doctype=json&version=1.1&q=' . urlencode($keyWord);
 		$result = json_decode(file_get_contents($url),true);
 		$info = array(
 				array($keyWord),
